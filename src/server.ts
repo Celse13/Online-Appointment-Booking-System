025faa7880a720a6a95c35a337695c67ebbfbee3
celse13 <rugiracelse13@gmail.com>
2024-05-21 15:15:38 +0200

    Connecting to server and MongoDB

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express,  { Request, Response, Express } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './database/connection';
+
+dotenv.config();
+
+
+const app: Express  = express();
+const PORT: number = parseInt(process.env.PORT as string, 10) || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}).catch((error) => {
+    console.error(`Failed to connect to the database: ${error}`);
+});
