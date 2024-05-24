3f7c7b94d7331332f4eb9d5ab80d7a6f35e96ba1
Dru Mumba <drusademumba@gmail.com>
2024-05-24 14:19:51 +0300

    Add about fragment

--- appointment/src/Landing/About/About.js
+++ appointment/src/Landing/About/About.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import { css } from 'aphrodite';
+import about from '../../Assets/about.png';
+import { aboutStyles } from '../../styles/landingStyles';
 
 class About extends React.Component {
   render() {
     return (
-      <div>
-        <h1>About</h1>
-      </div>
+      <Container className={css(aboutStyles.aboutContainer)}>
+        <Row>
+          <h1>ABOUT US</h1>
+          <Col md={6}></Col>
+          <Col md={6}>
+            <Card className={css(aboutStyles.aboutCard)}>
+              <img src={about} alt=''/>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
