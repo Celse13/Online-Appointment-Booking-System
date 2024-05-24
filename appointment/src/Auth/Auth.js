b1e4c0ba590d218b68473239d9f0ee94cc6049d4
Dru Mumba <drusademumba@gmail.com>
2024-05-24 21:49:42 +0300

    Add navigation

--- appointment/src/Auth/Auth.js
+++ appointment/src/Auth/Auth.js
@@ -5,6 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowCircleLeft } from '@fortawesome/free-solid-svg-icons';
 import { authStyles } from '../styles/authStyles';
 import Login from './Login/Login';
+import { withNavigate} from '../HOC/withNavigate';
 
 class Auth extends React.Component {
   constructor(props) {
@@ -16,16 +17,20 @@ class Auth extends React.Component {
     this.setState(prevState => ({ showLogin: !prevState.showLogin }));
   }
 
+  handleBackHome = () => {
+    this.props.navigate('/');
+  }
+
   render() {
     return (
       <div className={css(authStyles.auth)}>
-        <div className={css(authStyles.back)}>
-          <h3><FontAwesomeIcon icon={faArrowCircleLeft} className={css(authStyles.icon)}/> Go back home</h3>
+        <div className={css(authStyles.back)} onClick={this.handleBackHome}>
+          <h3><FontAwesomeIcon icon={faArrowCircleLeft} className={css(authStyles.icon)} /> Go back home</h3>
         </div>
-        { this.state.showLogin ? <Login toggleSignup={this.toggleForm} /> : <Signup toggleLogin={this.toggleForm} /> }
+        {this.state.showLogin ? <Login toggleSignup={this.toggleForm} /> : <Signup toggleLogin={this.toggleForm} />}
       </div>
     )
   }
 }
 
-export default Auth;
+export default withNavigate(Auth);
