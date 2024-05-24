e842f69417e6758264720e39adc82804073883eb
Dru Mumba <drusademumba@gmail.com>
2024-05-24 12:45:22 +0300

    Add files

---
+++ appointment/src/Landing/Testimonials/testimonials.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Testimonials extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Testimonials</h1>
+      </div>
+    );
+  }
+}
+
+export default Testimonials;
