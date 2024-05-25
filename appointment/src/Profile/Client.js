824e9126ade03291938421f4ae51061610733402
Dru Mumba <drusademumba@gmail.com>
2024-05-25 21:44:34 +0300

    Add files

---
+++ appointment/src/Profile/Client.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Client extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome, Client!</p>
+      </div>
+    );
+  }
+}
+
+export default Client;
