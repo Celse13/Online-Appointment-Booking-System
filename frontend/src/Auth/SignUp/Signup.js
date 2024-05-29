8ec7af50a40cc84725eceb51ae846bc3058f4811
Dru Mumba <drusademumba@gmail.com>
2024-05-30 00:48:13 +0300

    Add user role in signup

--- frontend/src/Auth/SignUp/Signup.js
+++ frontend/src/Auth/SignUp/Signup.js
@@ -7,9 +7,16 @@ import { Switch } from '../Switch';
 const Signup = (props) => {
   const [isToggled, setIsToggled] = useState(false);
 
-  const handleProfile = () => {
-    props.navigate('/profile/client');
+  const handleProfile = (event) => {
+    event.preventDefault();
+    const role = document.getElementById('role').value;
+    if (role === "1") {
+      props.navigate('/profile/admin');
+    } else {
+      props.navigate('/profile/client');
+    }
   }
+
 
   return (
     <div className={css(signStyles.signUpBody)}>
@@ -18,36 +25,34 @@ const Signup = (props) => {
         <h1>SIGN UP</h1>
         <div>
           <label htmlFor="name"></label>
-          <input type="text" name="name" id="name" autoComplete="true" placeholder="Name" className={css(signStyles.input)} />
+          <input type="text" name="name" id="name" autoComplete="true" placeholder="Name"
+                 className={css(signStyles.input)} />
         </div>
         <div>
           <label htmlFor="email"></label>
-          <input type="email" name="email" id="email" autoComplete="true" placeholder="Email" className={css(signStyles.input)} />
+          <input type="email" name="email" id="email" autoComplete="true" placeholder="Email"
+                 className={css(signStyles.input)} />
         </div>
         <div>
           <label htmlFor="password"></label>
-          <input type="password" name="password" id="password" autoComplete="true" placeholder="Password" className={css(signStyles.input)} />
+          <input type="password" name="password" id="password" autoComplete="true"
+                 placeholder="Password" className={css(signStyles.input)} />
         </div>
         <div>
           <label htmlFor="confirmPassword"></label>
-          <input type="password" name="confirmPassword" id="confirmPassword" autoComplete="true" placeholder="Confirm Password" className={css(signStyles.input)} />
+          <input type="password" name="confirmPassword" id="confirmPassword" autoComplete="true"
+                 placeholder="Confirm Password" className={css(signStyles.input)} />
         </div>
-        {isToggled && (
-          <div>
-            <label htmlFor="business"></label>
-            <select name="business" id="business" className={css(signStyles.input)}>
-              <option value="0">Select your service</option>
-              <option value="1">health</option>
-              <option value="2">fitness</option>
-              <option value="3">consultation</option>
-              <option value="4">Salon & Barber</option>
-              <option value="5">Massage & Spa</option>
-              <option value="6">Counselling</option>
-              <option value="7">Tuition</option>
-              <option value="8">Other</option>
-            </select>
-          </div>
-        )}
+        <div>
+          <label htmlFor="role"></label>
+          <select name="role" id="role" className={css(signStyles.input)}>
+            {isToggled ? (
+              <option value="1" role="biz">Business</option>
+            ) : (
+              <option value="2" role='client'>Client</option>
+              )}
+          </select>
+        </div>
         <input type="submit" value="SIGN UP" className={css(signStyles.button)}
                onClick={handleProfile} />
         <p><strong> Already have an account?
