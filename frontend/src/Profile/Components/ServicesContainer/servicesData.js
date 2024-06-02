e246d2522ea99571fc541474c39911f789471e2e
Dru Mumba <drusademumba@gmail.com>
2024-06-02 13:14:40 +0300

    separate opening time and closing time in service data

--- frontend/src/Profile/Components/ServicesContainer/servicesData.js
+++ frontend/src/Profile/Components/ServicesContainer/servicesData.js
@@ -19,17 +19,17 @@ export const serviceCategoriesData = [
 ];
 
 export const servicesListData = [
-  { id: 1, categoryId: 1, name: 'Tele-Medicine', duration: '30 minutes', cost: 50, workingHours: '9:00 AM - 5:00 PM', location: 'Online' },
-  { id: 2, categoryId: 2, name: 'Fitness by Aziz', duration: '120 minutes', cost: 100, workingHours: '6:00 AM - 10:00 PM', location: 'Smart Gym' },
-  { id: 3, categoryId: 3, name: 'MMS Firm', duration: '30 minutes', cost: 50, workingHours: '9:00 AM - 5:00 PM', location: 'MMS Head Office' },
-  { id: 4, categoryId: 4, name: 'Cutz', duration: '60 minutes', cost: 50, workingHours: '9:00 AM - 5:00 PM', location: 'Cutz Salon' },
-  { id: 5, categoryId: 5, name: 'Massage', duration: '1 hour', cost: 100, workingHours: '9:00 AM - 5:00 PM', location: 'Massage Parlor' },
-  { id: 6, categoryId: 6, name: 'Your Therapy Companion', duration: '60 minutes', cost: 100, workingHours: '11:00 AM - 11:00 PM', location: 'Therapy Center'},
-  { id: 7, categoryId: 7, name: 'Mathletes', duration: '60 minutes', cost: 50, workingHours: '9:00 AM - 4:00 PM', location: 'Home' },
-  { id: 8, categoryId: 5, name: 'Zen space', duration: '60 minutes', cost: 50, workingHours: '10:00 AM - 6:00 PM', location: 'Zen Space Center' },
-  { id: 9, categoryId: 8, name: 'Zap Electrics', duration: '60 minutes', cost: 50, workingHours: '7:00 AM - 5:00 PM', location: 'Home' },
-  { id: 10, categoryId: 7, name: 'Science Nerds', duration: '60 minutes', cost: 50, workingHours: '9:00 AM - 4:00 PM', location: 'Home' },
-  { id: 11, categoryId: 6, name: 'Children Therapy', duration: '60 minutes', cost: 50, workingHours: '9:00 AM - 5:00 PM', location: 'Therapy Center' },
-  { id: 12, categoryId: 4, name: 'Hair by T', duration: '60 minutes', cost: 50, workingHours: '9:00 AM - 5:00 PM', location: 'Hair by T Salon' },
+  { id: 1, categoryId: 1, name: 'Tele-Medicine', duration: '30 minutes', cost: 50, openingTime: '09:00', closingTime: '17:00', location: 'Online' },
+  { id: 2, categoryId: 2, name: 'Fitness by Aziz', duration: '120 minutes', cost: 100, openingTime: '06:00', closingTime: '10:00', location: 'Smart Gym' },
+  { id: 3, categoryId: 3, name: 'MMS Firm', duration: '30 minutes', cost: 50, openingTime: '09:00', closingTime: '17:00', location: 'MMS Head Office' },
+  { id: 4, categoryId: 4, name: 'Cutz', duration: '60 minutes', cost: 50, openingTime: '09:00', closingTime: '17:00', location: 'Cutz Salon' },
+  { id: 5, categoryId: 5, name: 'Massage', duration: '1 hour', cost: 100, openingTime: '09:00', closingTime: '17:00', location: 'Massage Parlor' },
+  { id: 6, categoryId: 6, name: 'Your Therapy Companion', duration: '60 minutes', cost: 100, openingTime: '11:00', closingTime: '11:00 ', location: 'Therapy Center'},
+  { id: 7, categoryId: 7, name: 'Mathletes', duration: '60 minutes', cost: 50, openingTime: '09:00', closingTime: '16:00', location: 'Home' },
+  { id: 8, categoryId: 5, name: 'Zen space', duration: '60 minutes', cost: 50, openingTime: '10:00', closingTime: '18:00', location: 'Zen Space Center' },
+  { id: 9, categoryId: 8, name: 'Zap Electrics', duration: '60 minutes', cost: 50, openingTime: '07:00', closingTime: '17:00', location: 'Home' },
+  { id: 10, categoryId: 7, name: 'Science Nerds', duration: '60 minutes', cost: 50, openingTime: '09:00', closingTime: '16:00', location: 'Home' },
+  { id: 11, categoryId: 6, name: 'Children Therapy', duration: '60 minutes', cost: 50, openingTime: '09:00', closingTime: '17:00', location: 'Therapy Center' },
+  { id: 12, categoryId: 4, name: 'Hair by T', duration: '60 minutes', cost: 50, openingTime: '09:00', closingTime: '17:00', location: 'Hair by T Salon' },
 ];
 
