a45d5da75215f649688f5754dc84e51634dd33a6
Dru Mumba <drusademumba@gmail.com>
2024-06-02 13:13:41 +0300

    Add get current date function

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -2,6 +2,11 @@ export const getFullYear = () =>{
   return new Date().getFullYear();
 }
 
+export const getCurrentDate = () => {
+  const today = new Date();
+  return today.toISOString().split('T')[0];
+};
+
 export const handleBackHome = (navigate) => {
   navigate('/');
 }
