deabdba6546b960747c06e4bb9d4a43bee577032
Dru Mumba <drusademumba@gmail.com>
2024-06-04 21:26:03 +0300

    Fix login page

--- frontend/src/Auth/Login/Login.js
+++ frontend/src/Auth/Login/Login.js
@@ -3,37 +3,29 @@ import { css } from 'aphrodite';
 import { signStyles } from '../../styles/authStyles';
 import { withNavigate } from '../../HOC/withNavigate';
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const Login = (props) => {
+  const handleProfile = (role) => role === "business" ? props.navigate('/profile/admin') : props.navigate('/profile/client');
 
-  handleProfile = (role) => {
-    role === "business" ? this.props.navigate('/profile/admin') : this.props.navigate('/profile/client');
-  }
-
-  render() {
-    return (
-      <div className={css(signStyles.signUpBody)}>
-        <form className={css(signStyles.form)}>
-          <h1>LOGIN</h1>
-          <div>
-            <label htmlFor="email"></label>
-            <input type="email" name="email" id="email" autoComplete="true" placeholder="Email" className={css(signStyles.input)} />
-          </div>
-          <div>
-            <label htmlFor="password"></label>
-            <input type="password" name="password" id="password" autoComplete="true" placeholder="Password" className={css(signStyles.input)} />
-          </div>
-          <input type="submit" value="LOGIN" className={css(signStyles.button)} onClick={() => handleProfile('business')}/>
-          <a href="#" className={css(signStyles.aLinks)}>Forgot Password</a>
-          <p><strong> No account?
-            <span className={css(signStyles.text)} onClick={this.props.toggleSignup}>Sign Up</span>
-          </strong></p>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div className={css(signStyles.signUpBody)}>
+      <form className={css(signStyles.form)} onSubmit={() => handleProfile( 'business')}>
+        <h1>LOGIN</h1>
+        <div>
+          <label htmlFor="email"></label>
+          <input type="email" name="email" id="email" autoComplete="true" placeholder="Email" className={css(signStyles.input)} />
+        </div>
+        <div>
+          <label htmlFor="password"></label>
+          <input type="password" name="password" id="password" autoComplete="true" placeholder="Password" className={css(signStyles.input)} />
+        </div>
+        <input type="submit" value="LOGIN" className={css(signStyles.button)}/>
+        <a href="#" className={css(signStyles.aLinks)}>Forgot Password</a>
+        <p><strong> No account?
+          <span className={css(signStyles.text)} onClick={props.toggleSignup}>Sign Up</span>
+        </strong></p>
+      </form>
+    </div>
+  );
 }
 
 export default withNavigate(Login);
