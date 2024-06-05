7850aa61bb4e40573fdbd15e72bcb1d089a446e4
Dru Mumba <drusademumba@gmail.com>
2024-06-05 10:40:05 +0300

    Change phone and location inputs

--- frontend/src/Auth/SignUp/Signup.js
+++ frontend/src/Auth/SignUp/Signup.js
@@ -4,18 +4,11 @@ import { signStyles } from '../../styles/authStyles';
 import { withNavigate } from '../../HOC/withNavigate';
 import { Switch } from '../Switch';
 import { Lock, Mail, Phone, Pin, UserRound, UserRoundCog } from 'lucide-react';
-import PhoneInput from 'react-phone-input-2';
 import 'react-phone-input-2/lib/style.css';
-
 import axios from 'axios';
-
 
 const Signup = (props) => {
   const [isToggled, setIsToggled] = useState(false);
-  const [phone, setPhone] = useState('');
-  const [location, setLocation] = useState('');
-
-
   const BASE_URL = 'http://localhost:5500/api/auth/signup'
 
   const handleProfile = async (event) => {
@@ -25,16 +18,13 @@ const Signup = (props) => {
     const password = document.getElementById('password').value;
     const confirmPassword = document.getElementById('confirmPassword').value;
     const role = isToggled ? 'business' : 'client';
-    const phone = isToggled ? phone : null;
-    const location = isToggled ? location : null;
+    const phone = isToggled ? document.getElementById('phone').value : null;
+    const location = isToggled ? document.getElementById('location').value : null;
     const description = isToggled ? document.getElementById('description').value : null;
-
-
     if (password !== confirmPassword) {
       alert('Passwords do not match!');
       return;
     }
-
 
     let user = {
       name: name,
@@ -43,19 +33,17 @@ const Signup = (props) => {
       role: role
     };
 
-
     if (role === 'business') {
       user.phone = phone;
       user.location = location;
       user.description = description;
     }
 
-
     try {
       const response = await axios.post(BASE_URL, user);
-      console.log(response.data);
       role === "business" ? props.navigate('/profile/admin') : props.navigate('/profile/client');
     } catch (error) {
+      alert('Error creating user. Try again later!');
       console.error(error);
     }
   }
@@ -68,48 +56,47 @@ const Signup = (props) => {
         <div>
           <span><UserRound />
             <input type="text" name="name" id="name" autoComplete="true" placeholder="Name"
-              className={css(signStyles.input)} required />
+                   className={css(signStyles.input)} required />
           </span>
         </div>
         <div>
           <span><Mail />
             <input type="email" name="email" id="email" autoComplete="true" placeholder="Email"
-              className={css(signStyles.input)} required />
+                   className={css(signStyles.input)} required />
           </span>
         </div>
         <div>
           <span><Lock />
             <input type="password" name="password" id="password" autoComplete="true"
-              placeholder="Password" className={css(signStyles.input)} required />
+                   placeholder="Password" className={css(signStyles.input)} required />
           </span>
         </div>
         <div>
           <span><Lock />
             <input type="password" name="confirmPassword" id="confirmPassword" autoComplete="true"
-              placeholder="Confirm Password" className={css(signStyles.input)} required />
+                   placeholder="Confirm Password" className={css(signStyles.input)} required />
           </span>
         </div>
         <div>
           <span><UserRoundCog />
             <input type="text" name="role" id="role" value={isToggled ? 'business' : 'client'}
-              className={css(signStyles.input)} readOnly />
+                   className={css(signStyles.input)} readOnly />
           </span>
         </div>
         {isToggled && (
           <div>
             <span><Phone />
-              <PhoneInput country={'ke'} value={phone} onChange={phone => setPhone(phone)}
-                inputStyle={{ width: '100%' }}
-                className={css(signStyles.input)} />
+              <input type="text" name="phone" id="phone"
+                     autoComplete="true" placeholder="phone"
+                     className={css(signStyles.input)} required />
             </span>
             <span><Pin />
               <input type="text" name="location" id="location" autoComplete="true"
-                placeholder="Location"
-                className={css(signStyles.input)} value={location}
-                onChange={(e) => setLocation(e.target.value)} required />
+                     placeholder="location"
+                     className={css(signStyles.input)} required />
             </span>
             <textarea name="description" id="description" placeholder="Business Description"
-              className={css(signStyles.input)} />
+                      className={css(signStyles.input)} />
           </div>
         )}
         <input type="submit" value="SIGN UP" className={css(signStyles.button)} />
