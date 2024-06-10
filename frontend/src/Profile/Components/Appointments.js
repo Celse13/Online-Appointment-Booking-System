9611f1fe9adb2476b96929ef93480df7c6f1d939
celse13 <rugiracelse13@gmail.com>
2024-06-10 11:20:02 +0200

    Changes on the appointment

--- frontend/src/Profile/Components/Appointments.js
+++ frontend/src/Profile/Components/Appointments.js
@@ -38,7 +38,7 @@ const Appointments = () => {
         const response = await getAppointments(token);
         const appointments = response.appointments.map((appointment, index) => ({
           id: index + 1,
-          name: role === 'business' ? `Client: ${appointment.clientName}` : `Appointment with: ${appointment.serviceName}` ,
+          name: role === 'business' ? `Client: ${appointment.clientName}` : `Appointment with: ${appointment.service[0].name}` ,
           date: new Date(appointment.dateTime).toLocaleDateString(),
           time: formatTime(new Date(appointment.dateTime).toLocaleTimeString()),
           location: appointment.service[0].location,
