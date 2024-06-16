f8aeb98235e028c883e9b9bcf7ee325e77f55def
celse13 <rugiracelse13@gmail.com>
2024-06-16 19:57:15 +0200

    Integrating the clients list

--- frontend/src/Profile/Components/ClientsList.js
+++ frontend/src/Profile/Components/ClientsList.js
@@ -1,25 +1,50 @@
-import React from 'react';
-import { Card, Container } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Card, Table, Container, Spinner } from 'react-bootstrap';
 import { css } from 'aphrodite';
 import { clientsListStyles } from '../../styles/profCompStyles';
-
-const clientsData = [
-  { id: 1, name: 'Client 1' },
-  { id: 2, name: 'Client 2' },
-  { id: 3, name: 'Client 3' },
-];
+import ClientApiHandler from "../../Api/Clients/handleClientsApi";
 
 const ClientsList = () => {
+  const [clients, setClients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchClients = async () => {
+      setIsLoading(true);
+      const token = localStorage.getItem("token");
+      const data = await ClientApiHandler.getClients(token);
+      setClients(data.clients);
+      setIsLoading(false);
+    };
+
+    fetchClients();
+  }, []);
+
+  if (isLoading) {
+    return <Spinner animation="border" />;
+  }
+
   return (
     <Container className={css(clientsListStyles.container)}>
       <h3>Client List</h3>
-      <Card className={css(clientsListStyles.card)}>
-        {clientsData.map((client) => (
-          <h5 key={client.id} className={css(clientsListStyles.text)}>
-            {client.name}
-          </h5>
-        ))}
-      </Card>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Client ID</th>
+            <th>Number of Appointments</th>
+          </tr>
+        </thead>
+        <tbody>
+          {clients.map((client, index) => (
+            <tr key={client._id}>
+              <td>{index + 1}</td>
+              <td>{client.client}</td>
+              <td>{client.appointments.length}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </Container>
   );
 }
